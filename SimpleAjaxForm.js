84dd60f5d9cd9912466cf882379550cdf4566de3
Kevin Friend <kevin.wood.friend@gmail.com>
2019-10-28 16:10:47 -0400

    Abstracted module name and event namespace

--- SimpleAjaxForm.js
+++ SimpleAjaxForm.js
@@ -1,3 +1,6 @@
+const moduleName = 'SimpleAjaxForm';
+const eventNamespace = moduleName.toLocaleLowerCase();
+
 const entities = {
     '&': '&amp;',
     '<': '&lt;',
@@ -68,11 +71,11 @@ class SimpleAjaxForm {
         }
 
         if (!el || (el instanceof Element) === false) {
-            throw new Error('SimpleAjaxForm: Invalid target element supplied')
+            throw new Error(`${moduleName}: Invalid target element supplied`)
         }
 
         if (!action) {
-            throw new Error('SimpleAjaxForm: Invalid `action` argument supplied');
+            throw new Error(`${moduleName}: Invalid 'action' argument supplied`);
         }
 
         this.action = action;
@@ -117,7 +120,7 @@ class SimpleAjaxForm {
             this.options.submit();
         }
 
-        this.el.dispatchEvent(event('simpleajaxform.submit'));
+        this.el.dispatchEvent(event(`${eventNamespace}.submit`));
 
         let requestOpts = {
             method: 'POST',
@@ -183,7 +186,7 @@ class SimpleAjaxForm {
             this.messages.scrollIntoView({ behavior: "smooth" });
         }
 
-        this.el.dispatchEvent(event('simpleajaxform.success', ...arguments));
+        this.el.dispatchEvent(event(`${eventNamespace}.success`, ...arguments));
     }
 
     handleError(response) {
@@ -211,7 +214,7 @@ class SimpleAjaxForm {
             this.messages.scrollIntoView({ behavior: "smooth" });
         }
 
-        this.el.dispatchEvent(event('simpleajaxform.error', arguments));
+        this.el.dispatchEvent(event(`${eventNamespace}.error`, arguments));
     }
 
     handleComplete() {
@@ -223,7 +226,7 @@ class SimpleAjaxForm {
             this.options.complete();
         }
 
-        this.el.dispatchEvent(event('simpleajaxform.complete'));
+        this.el.dispatchEvent(event(`${eventNamespace}.complete`));
     }
 
     generateErrors(messages) {
